47657c6177e8b1df281e5bfb66be2d25a86af08a
Rishi Shah <rishihahs@gmail.com>
2014-07-08 14:37:54 -0500

    Implement ChromeCast Methods

--- index.js
+++ index.js
@@ -7,15 +7,34 @@ castify = (function () {
     var chrome = require('./lib/cast_sender'),
         that = {},
         retries = 0, // Chromecast connection retries
+        onAvailable = null, // Callback when ChromeCast is available
+        session = null, // chrome.cast.Session
+        currentMedia = null, // chrome.cast.media.Media
 
         // Chromecast Listeners
         sessionListener = function () {
             console.log('Session Listener');
         },
 
+        onRequestSessionSuccess = function (e) {
+            session = e;
+            onAvailable();
+        },
+
+        onLaunchError = function () {
+            throw {
+                name: 'LaunchError',
+                message: 'Could not request session'
+            };
+        },
+
         receiverListener = function (e) {
             if (e === chrome.cast.ReceiverAvailability.AVAILABLE) {
                 console.log('Receiver Listener: Available');
+
+                if (typeof onAvailable === 'function') {
+                    chrome.cast.requestSession(onRequestSessionSuccess, onLaunchError);
+                }
             } else {
                 console.log('Receiver Listener: Not Available');
             }
@@ -32,8 +51,10 @@ castify = (function () {
             };
         };
 
-    that.initialize = function () {
-        var appID, session, config;
+    that.initialize = function (onAvailableCallback) { // onAvailableCallback called when receiver listner is available
+        var appID, sessionRequest, config;
+
+        onAvailable = onAvailableCallback;
 
         // Check if chromecast is available
         // retry if not
@@ -55,11 +76,40 @@ castify = (function () {
         // Initialize Chromecast
         appID = chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID;
 
-        session = new chrome.cast.SessionRequest(appID);
-        config = new chrome.cast.ApiConfig(session, sessionListener, receiverListener);
+        sessionRequest = new chrome.cast.SessionRequest(appID);
+        config = new chrome.cast.ApiConfig(sessionRequest, sessionListener, receiverListener);
 
         chrome.cast.initialize(config, onInitSuccess, onInitError);
 
+    };
+
+    that.playMedia = function (currentMediaURL) {
+        var mediaInfo, request, onMediaDiscovered, onMediaError;
+
+        if (!session) {
+            throw {
+                name: 'PrematureInvocationError',
+                message: 'ChromeCast has not yet been initialized'
+            };
+        }
+
+        onMediaDiscovered = function (how, media) {
+            console.log('Media discovered ' + how);
+            currentMedia = media;
+        };
+
+        onMediaError = function () {
+            throw {
+                name: 'MediaError',
+                message: 'Error starting media'
+            };
+        };
+
+        mediaInfo = new chrome.cast.media.MediaInfo(currentMediaURL);
+        request = new chrome.cast.media.LoadRequest(mediaInfo);
+        session.loadMedia(request,
+            onMediaDiscovered.bind(this, 'loadMedia'),
+            onMediaError);
     };
 
     return that;
